d7c21fa216e37cacddf304c0e9aec906e3d38d1e
bogdan_info <bogdan_info@mail.ru>
2021-05-10 01:44:17 +0300

    decomposition component InstallationRp.js

--- client/src/Components/InstallationRp/InstallationRp.js
+++ client/src/Components/InstallationRp/InstallationRp.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import classes from './InstallationRp.module.css'
+import CalculationGi from "../СalculationGi/CalculationGi";
 
 const InstallationRp = ({
                             pzFrom,
@@ -63,68 +64,21 @@ const InstallationRp = ({
             </div>
             <hr/>
             <div className={classes.flexColumn}>
-                <div className={`row ${classes.marginBottom}`}>
-                    <div className="input-field col s4">
-                        <input
-                            id="email-3"
-                            type="number"
-                            autoComplete={'off'}
-                            onChange={(event) => calculationGi('From', pzFrom.h, event.target.value, keyH)}
-                        />
-                        <label htmlFor="email-3">отсчет</label>
-                        <span className="helper-text" data-error="wrong" data-success="right">{pzFrom.number}</span>
-                    </div>
-                    <div className={'col s8'}>
-                        <div className={`${classes.Wrap}`}>
-                            <p>ГИ-</p>
-                            <p>{pzFromGi}</p>
-                            <label>
-                                <input
-                                    id="indeterminate-checkbox"
-                                    type="checkbox"
-                                    checked={exactFrom}
-                                    onChange={event => {
-                                        calculationRpList('From', rpList, pzFromGi, averageGi, exactFrom, exactBefore, countdownRp, itemClass)
-                                    }
-                                    }
-                                />
-                                <span>Принять</span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <p className={`${classes.marginNull} ${classes.textAlign}`}>Для среднего значения примите оба ГИ</p>
-                <div className={`row ${classes.marginBottom}`}>
-                    <div className="input-field col s4">
-                        <input
-                            id="email-4"
-                            type="number"
-                            autoComplete={'off'}
-                            onChange={(event) => calculationGi('Before', pzBefore.h, event.target.value, keyH)}
-
-                        />
-                        <label htmlFor="email-4">отсчет</label>
-                        <span className="helper-text" data-error="wrong" data-success="right">{pzBefore.number}</span>
-                    </div>
-                    <div className={'col s8'}>
-                        <div className={`${classes.Wrap}`}>
-                            <p>ГИ-</p>
-                            <p>{pzBeforeGi}</p>
-                            <label>
-                                <input
-                                    id="indeterminate-checkbox"
-                                    type="checkbox"
-                                    checked={exactBefore}
-                                    onChange={event => {
-                                        calculationRpList('Before', rpList, pzBeforeGi, averageGi, exactFrom, exactBefore, countdownRp, itemClass)
-                                    }
-                                    }
-                                />
-                                <span>Принять</span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
+                <CalculationGi
+                    calculationGi={calculationGi}
+                    pzFrom={pzFrom}
+                    keyH={keyH}
+                    pzFromGi={pzFromGi}
+                    exactFrom={exactFrom}
+                    calculationRpList={calculationRpList}
+                    rpList={rpList}
+                    averageGi={averageGi}
+                    exactBefore={exactBefore}
+                    countdownRp={countdownRp}
+                    itemClass={itemClass}
+                    pzBeforeGi={pzBeforeGi}
+                    pzBefore={pzBefore}
+                />
                 <div className={classes.flexColumn}>
                     {contentInit(countdownRp, itemClass)}
                 </div>
---
+++ client/src/Components/СalculationGi/CalculationGi.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import classes from "../InstallationRp/InstallationRp.module.css";
+
+const CalculationGi = ({
+                           calculationGi,
+                           pzFrom,
+                           keyH,
+                           pzFromGi,
+                           exactFrom,
+                           calculationRpList,
+                           rpList,
+                           averageGi,
+                           exactBefore,
+                           countdownRp,
+                           itemClass,
+                           pzBeforeGi,
+                           pzBefore
+                       }) => {
+    return (
+        <>
+            <div className={`row ${classes.marginBottom}`}>
+                <div className="input-field col s4">
+                    <input
+                        id="email-3"
+                        type="number"
+                        autoComplete={'off'}
+                        onChange={(event) => calculationGi('From', pzFrom.h, event.target.value, keyH)}
+                    />
+                    <label htmlFor="email-3">отсчет</label>
+                    <span className="helper-text" data-error="wrong" data-success="right">{pzFrom.number}</span>
+                </div>
+                <div className={'col s8'}>
+                    <div className={`${classes.Wrap}`}>
+                        <p>ГИ-</p>
+                        <p>{pzFromGi}</p>
+                        <label>
+                            <input
+                                id="indeterminate-checkbox"
+                                type="checkbox"
+                                checked={exactFrom}
+                                onChange={event => {
+                                    calculationRpList('From', rpList, pzFromGi, averageGi, exactFrom, exactBefore, countdownRp, itemClass)
+                                }
+                                }
+                            />
+                            <span>Принять</span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <p className={`${classes.marginNull} ${classes.textAlign}`}>Для среднего значения примите оба ГИ</p>
+            <div className={`row ${classes.marginBottom}`}>
+                <div className="input-field col s4">
+                    <input
+                        id="email-4"
+                        type="number"
+                        autoComplete={'off'}
+                        onChange={(event) => calculationGi('Before', pzBefore.h, event.target.value, keyH)}
+
+                    />
+                    <label htmlFor="email-4">отсчет</label>
+                    <span className="helper-text" data-error="wrong" data-success="right">{pzBefore.number}</span>
+                </div>
+                <div className={'col s8'}>
+                    <div className={`${classes.Wrap}`}>
+                        <p>ГИ-</p>
+                        <p>{pzBeforeGi}</p>
+                        <label>
+                            <input
+                                id="indeterminate-checkbox"
+                                type="checkbox"
+                                checked={exactBefore}
+                                onChange={event => {
+                                    calculationRpList('Before', rpList, pzBeforeGi, averageGi, exactFrom, exactBefore, countdownRp, itemClass)
+                                }
+                                }
+                            />
+                            <span>Принять</span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CalculationGi
