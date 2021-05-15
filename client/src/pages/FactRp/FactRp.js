7563eff562b181a9728f983816f2e75799c41629
bogdan_info <bogdan_info@mail.ru>
2021-05-16 02:40:04 +0300

    before installing the material-ui

--- client/src/pages/FactRp/FactRp.js
+++ client/src/pages/FactRp/FactRp.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import NavBarMenu from "../../Components/NavBarMenu/NavBarMenu";
+import {connect} from 'react-redux'
+import classes from './FactRp.module.css'
 
 
 
-const FactRp = () => {
+const FactRp = (props) => {
     return (
-        <>
+        <div className={classes.fact}>
             <NavBarMenu
                 btnName={["Главная", 'Репера', 'Рихтовка', "Назад"]}
                 linkTo={['/', '/rp', '/', '/options']}
             />
-        </>
+            <h1>{props.title}</h1>
+        </div>
     )
 }
 
-export default FactRp
+function mapStateToProps(state) {
+    return {
+        title: state.objectListReducer.currentObject.title
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(FactRp)
