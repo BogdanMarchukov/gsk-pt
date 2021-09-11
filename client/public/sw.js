8e8b70eb55176d2475f2b1602d75a9cf94b7a3c1
bogdan_info <bogdan_info@mail.ru>
2021-09-12 00:44:39 +0300

    servesWorker - fix bag v.1

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-app - v1'
-const dynamicCacheName = 'dynamic - v2'
+const staticCacheName = 'static-app - v2'
+const dynamicCacheName = 'dynamic - v3'
 const staticUrl = [
     'index.html',
     '/static/js/bundle.js',
@@ -27,10 +27,12 @@ self.addEventListener('fetch', event => {
 
     const {request} = event
     const url = new URL(request.url)
-    if (url.origin === location.origin && url.pathname !== '/api/fetch') {
+
+    if (url.pathname === '/') {
         event.respondWith(cacheFirst(request))
     } else {
         event.respondWith(networkFirst(request))
+
     }
 
 
@@ -45,11 +47,12 @@ async function networkFirst(req) {
     const cache = await caches.open(dynamicCacheName)
     try {
         const response = await fetch(req)
-        console.log(response)
+
         await cache.put(req, response.clone())
-        console.log(response)
+
         return response
     } catch (e) {
+        console.log('zzz')
             // TODO прроверить этот блок
         const cachedRes = await cache.match(req)
         return cachedRes
